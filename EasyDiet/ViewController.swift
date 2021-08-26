c392afdb637a57f0eb9551db6fbcdcaa7428085e
Cavok93 <fire93sim@naver.com>
2021-08-26 22:34:40 +0900

    Initial Commit

---
+++ EasyDiet/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EasyDiet
+//
+//  Created by 심정섭 on 2021/08/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
